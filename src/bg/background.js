cff05a2f2fef6ccc9d407b39ba93d34c00b0c6e9
sapslaj <saps.laj@gmail.com>
2013-03-31 17:37:04 -0400

    Added options to main logic

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,17 +1,25 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
+// Main logic
 chrome.browserAction.onClicked.addListener(function(tab)
 {
-	var nopeurl = "http://no.pe";
-	chrome.tabs.update({url: nopeurl, active: true});
-	
-	var now = (new Date).getTime();
-	var minutesago = now - 600000;
-	chrome.history.deleteRange({startTime: minutesago, endTime: now}, function(){});
+    redirect();
+    
+	if(localStorage["ckb_enableHistoryNuke"])
+	   nukehist();
 });
+
+function redirect()
+{
+    chrome.tabs.update({url: localStorage["nopeurl"], active: true});
+}
+
+function nukehist()
+{
+    if(localStorage["ckb_nukeEverything"] == "checked")
+        chrome.history.deleteAll(function(){});
+    else
+    {
+        var now = (new Date).getTime();
+        var minutesago = now - localStorage["HistDelAmount"];
+        chrome.history.deleteRange({startTime: minutesago, endTime: now}, function(){});
+    }
+}
